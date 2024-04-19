21c66401be50c1bc86b6c4569ae67b3cad5aa362
isabellagoes <isabellagoes.igs@gmail.com>
2024-04-18 22:34:33 -0300

    .

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let nota2 = parseInt(prompt("Digite a segunda nota "));
 let nota3 = parseInt(prompt("Digite a terceira nota "));
 let nota4 = parseInt(prompt("Digite a quarta nota "));
 
-let media =parseFloat(((nota1 + nota2 + nota3 + nota4)/2).toFixed(1));
+let media = parseFloat(((nota1 + nota2 + nota3 + nota4)/2).toFixed(1));
 
 if (condition) {
 
