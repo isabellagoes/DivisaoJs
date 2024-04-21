e788e3f6c0e436a480e96563d99cfb53db6b1e0a
isabellagoes <isabellagoes.igs@gmail.com>
2024-04-21 17:14:02 -0300

    .

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-let nota1 = parseInt(prompt("Digite a primeira nota "));
-let nota2 = parseInt(prompt("Digite a segunda nota "));
-let nota3 = parseInt(prompt("Digite a terceira nota "));
-let nota4 = parseInt(prompt("Digite a quarta nota "));
+// Console.Write("Digite o numerador: ");
+// double numerador = Convert.ToDouble(Console.ReadLine());
 
-let media = parseFloat(((nota1 + nota2 + nota3 + nota4)/2).toFixed(1));
+// Console.Write("Digite o denominador: ");
+// double denominador = Convert.ToDouble(Console.ReadLine());
 
-if (condition) {
+// if (denominador == 0)
+// {
+//     Console.Write("Não é possível dividir por zero.");
+// }
 
+// else
+// {
+//     double quociente = numerador / denominador;
+//     Console.Write($"\n{numerador:N1} dividido por {denominador:N1} é {quociente:N1}.");
+// }
+
+let numerador = parseInt(prompt("Digite o numerador: "));
+let denominador = parseInt(prompt("Digite o denominador: "));
+
+if (denominador == 0) {
+    alert("Não é possível dividir por zero.");
 }
+else {
+    let quociente = numerador / denominador;
 
-alert(media);
+    alert(numerador + " " + "dividido por " + denominador + " é " + quociente);
+}
